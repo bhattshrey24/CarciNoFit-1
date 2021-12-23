d750da992dc3821f06d384cab2d646d5d4758770
shrey <bhatt.shrey1224@gmail.com>
2021-12-23 17:58:23 +0530

    result data changes

--- app/src/main/java/com/example/carcinofit/database/models/ResultDataClass.kt
+++ app/src/main/java/com/example/carcinofit/database/models/ResultDataClass.kt
@@ -6,7 +6,12 @@ class ResultDataClass {
         ResultDataClassStructure(0.0625f, 0.9375f, arrayOf("Carbromal","Benzofuran"), 3000L),// tot 32
         ResultDataClassStructure(0.14f, 0.86f, arrayOf("Anilazine","Acrolein","Sulfisoxazole","Sulfallate"), 4500L),// tot 28
         ResultDataClassStructure(0.048f, 0.952f, arrayOf("Diazinon","Iodoform"), 5600L),// tot 41
-        ResultDataClassStructure(0.027f, 0.973f, arrayOf("Saccharin"), 6500L)// tot 36
+        ResultDataClassStructure(0.027f, 0.973f, arrayOf("Saccharin"), 6500L),// tot 36
+
+
+        ResultDataClassStructure(0.0f, 0.1f, arrayOf("Non Found"), 8000L),// tot 36
+        ResultDataClassStructure(0.0f, 0.1f, arrayOf("Non Found"), 4000L)// tot 36
+
     )
 //    ResultDataClassStructure(0.11f, 0.89f, arrayOf("Hematoxylin,Saccharin,Retrorsine,Triethanolamine"), 7000L)
 //    ResultDataClassStructure(0.01f, 0.99f, arrayOf("Diazinon,Dulcin,Iodoform"), 5000L),
